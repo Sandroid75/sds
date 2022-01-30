e687592c0ea19a344a440de3087e9345996a5c77
Sandroid75 <mazzarosandro@gmail.com>
2022-01-30 17:06:40 +0100

    clang format extra

--- sds_extra.c
+++ sds_extra.c
@@ -31,10 +31,10 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #include "sds.h"
 #include "sds_extra.h"
@@ -55,24 +55,24 @@ int sdssds(const sds haystack, const sds needle) {
     haystack_len = sdslen(haystack);
     needle_len = sdslen(needle);
 
-    if(!haystack_len || !needle_len || haystack_len < needle_len) { //if one or both of two sds are empty return -1 also if haystack lenght is less than needle lenght
-      return -1;    //also if both are 0 lenght for me there is no string to compare
+    if (!haystack_len || !needle_len || haystack_len < needle_len) { // if one or both of two sds are empty return -1 also if haystack lenght is less than needle lenght
+        return -1;                                                   // also if both are 0 lenght for me there is no string to compare
     }
 
-    if(haystack_len == needle_len) { //check if haystack and needle have the same lenght
-        return (sdscmp(haystack, needle) ? -1 : 0); //return 0 if are the same otherwise -1
+    if (haystack_len == needle_len) {               // check if haystack and needle have the same lenght
+        return (sdscmp(haystack, needle) ? -1 : 0); // return 0 if are the same otherwise -1
     }
 
-    chr = needle[0]; //get the first char of needle
-    for(ptr = memchr(haystack, chr, haystack_len); ptr != NULL; ptr = memchr(ptr, chr, ptr_len)) { //initializing for with the first occurrence of chr; check if is valid ptr; continuing with next occurrence
-        if(!memcmp(ptr, needle, needle_len)) { //compare the ptr with match for needle lenght
-            return 1;           //a match was found!
+    chr = needle[0];                                                                                // get the first char of needle
+    for (ptr = memchr(haystack, chr, haystack_len); ptr != NULL; ptr = memchr(ptr, chr, ptr_len)) { // initializing for with the first occurrence of chr; check if is valid ptr; continuing with next occurrence
+        if (!memcmp(ptr, needle, needle_len)) {                                                     // compare the ptr with match for needle lenght
+            return 1;                                                                               // a match was found!
         }
-        ptr++; //increase one char to step over the previous founded char
-        ptr_len = strlen(ptr); //get the new lenght of ptr
+        ptr++;                 // increase one char to step over the previous founded char
+        ptr_len = strlen(ptr); // get the new lenght of ptr
     }
 
-    return -1; //no match found
+    return -1; // no match found
 }
 
 /* The sdscasesds() function finds the first occurrence of the substring needle
@@ -88,17 +88,17 @@ int sdscasesds(const sds haystack, const sds needle) {
     sds string, match;
     int result;
 
-    string = sdsdup(haystack);  //duplicate haystack
-    match = sdsdup(needle);     //duplicate needle
-    sdstolower(string);         //convert to lovwercase string
-    sdstolower(match);          //convert to lowercase match
+    string = sdsdup(haystack); // duplicate haystack
+    match = sdsdup(needle);    // duplicate needle
+    sdstolower(string);        // convert to lovwercase string
+    sdstolower(match);         // convert to lowercase match
 
-    result = sdssds(string, match); //all is lowercase than can call sdssds
+    result = sdssds(string, match); // all is lowercase than can call sdssds
 
-    sdsfree(string);    //destroy the sds
-    sdsfree(match);     //destroy the sds
+    sdsfree(string); // destroy the sds
+    sdsfree(match);  // destroy the sds
 
-    return result; //no match found
+    return result; // no match found
 }
 
 /* The sdschremove() function remove all single char occurrence
@@ -113,34 +113,34 @@ sds sdschremove(sds s, char *cset) {
     char *ptr, *src, *dst, *purgedstr;
     size_t newlen = 0, len = sdslen(s);
 
-    if(!len || !cset || !strlen(cset)) { //check if there is something to remove
+    if (!len || !cset || !strlen(cset)) { // check if there is something to remove
         return s;
     }
 
-    purgedstr = (char *) malloc(sdslen(s) +1); //allocate memory for new string
-    dst = purgedstr; //dst point to new empty string
-    src = s; //src point to original sds string
+    purgedstr = (char *)malloc(sdslen(s) + 1); // allocate memory for new string
+    dst = purgedstr;                           // dst point to new empty string
+    src = s;                                   // src point to original sds string
 
-    for(int i = 0; i < len; i++) { //walk to all binary 's' sds string
-        for(ptr = cset; *ptr && i < len; ptr++) { //walk to all NULL terminated 'cset' string and up to the end of binary sds
-            if(*src == *ptr) {
-                src++; //current ch is present in cset than skip to next
-                i++; //increment the index of binary sds
-                ptr = cset; //reset the ptr pointer to beguin of cset string
+    for (int i = 0; i < len; i++) {                // walk to all binary 's' sds string
+        for (ptr = cset; *ptr && i < len; ptr++) { // walk to all NULL terminated 'cset' string and up to the end of binary sds
+            if (*src == *ptr) {
+                src++;      // current ch is present in cset than skip to next
+                i++;        // increment the index of binary sds
+                ptr = cset; // reset the ptr pointer to beguin of cset string
             }
         }
-        if(i < len) { //check if the sds binary string is over
-            *dst = *src; //the current char of scr is not present in cset string
-            dst++; //increment the dst pointer
-            src++; //increment the src pointer
-            newlen++; //increment the count of newlen
+        if (i < len) {   // check if the sds binary string is over
+            *dst = *src; // the current char of scr is not present in cset string
+            dst++;       // increment the dst pointer
+            src++;       // increment the src pointer
+            newlen++;    // increment the count of newlen
         }
     }
 
-    sdsfree(s); //destroy original sds binary string
-    s = sdsnewlen(purgedstr, newlen); //create the ner sds binary string purged from 'cset' char
-    if(purgedstr) { //check purgedstr
-        free(purgedstr); //release the memory
+    sdsfree(s);                       // destroy original sds binary string
+    s = sdsnewlen(purgedstr, newlen); // create the ner sds binary string purged from 'cset' char
+    if (purgedstr) {                  // check purgedstr
+        free(purgedstr);              // release the memory
     }
 
     return s;
@@ -159,25 +159,23 @@ sds sdscasechremove(sds s, char *cset) {
     size_t len = strlen(cset);
     int i;
 
-    ignorecase = (char *) malloc(len +1); //allocate memory for ignorecase string
+    ignorecase = (char *)malloc(len + 1); // allocate memory for ignorecase string
 
-    for(i = 0; i < len; i++) {
-        ignorecase[i] = tolower(cset[i]); //convert cset to lowercase
+    for (i = 0; i < len; i++) {
+        ignorecase[i] = tolower(cset[i]); // convert cset to lowercase
     }
     ignorecase[len] = '\0';
-    s = sdschremove(s, ignorecase); //call the sdschremove function with lowercase
-    
-    for(i = 0; i < len; i++) {
-        ignorecase[i] = toupper(cset[i]); //convert cset to uppercase
+    s = sdschremove(s, ignorecase); // call the sdschremove function with lowercase
+
+    for (i = 0; i < len; i++) {
+        ignorecase[i] = toupper(cset[i]); // convert cset to uppercase
     }
     ignorecase[len] = '\0';
-    s = sdschremove(s, ignorecase); //call the sdschremove function with uppercase
-    
-    if(ignorecase) { //check ignorecase
-        free(ignorecase); //release the memory
+    s = sdschremove(s, ignorecase); // call the sdschremove function with uppercase
+
+    if (ignorecase) {     // check ignorecase
+        free(ignorecase); // release the memory
     }
 
     return s;
 }
-
-
